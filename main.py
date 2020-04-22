f0beec5f13bca23f01c5a52a9080d5f2d8cc6928
Arthur Pesah <arthur.pesah@gmail.com>
2020-04-22 02:01:29 +0200

    Introduce SMPC, improve averaging process

---
+++ main.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+import syft as sy
+import torchvision.datasets as datasets
+from torchvision import transforms
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
